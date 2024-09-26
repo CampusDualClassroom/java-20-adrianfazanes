56f6571bc1845e0e2b5ab5b35dfdf2a49355db7e
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-26 10:24:29 +0200

    Ejercicio 20 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -5,19 +5,27 @@ import java.util.List;
 
 public class Exercise20 {
 
+    // Crear una lista de personas y sus derivados
     public static List<Person> getPeopleList() {
+        List<Person> peopleList = new ArrayList<>();
 
+        peopleList.add(new Person("John", "Smith"));
+        peopleList.add(new Teacher("María", "Montessori", "Educación"));
+        peopleList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        peopleList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+
+        return peopleList;
     }
 
-    public static void showPeopleDetails(List<Person> stringList) {
 
+    public static void showPeopleDetails(List<Person> peopleList) {
+        for (Person person : peopleList) {
+            person.getDetails();
+        }
     }
 
     public static void main(String[] args) {
-        List<Person> stringList = getPeopleList();
-        showPeopleDetails(stringList);
+        List<Person> peopleList = getPeopleList();
+        showPeopleDetails(peopleList);
     }
-
-
-
 }
